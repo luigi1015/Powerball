f1baee39ffe8a07c943fada4f20817d5233946b4
luigi1015 <github@314ghz.com>
2014-01-12 19:25:52 -0600

    Added a couple methods to PBNum.

--- src/net/codehobby/PBNum.java
+++ src/net/codehobby/PBNum.java
@@ -5,4 +5,14 @@ public class PBNum {
 	private int month, day, year;//Date the number was drawn.
 	private int type;//The type of number drawn. See the PowerballTypes enum for a list of types.
 
+	public int getNumber()
+	{//Returns number.
+		return num;
+	}
+
+	public void setNumber( int newNum )
+	{//Set Number.
+		num = newNum;
+	}
+
 }

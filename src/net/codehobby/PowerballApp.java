cc4af25ba60bf3d7a92faa89fc041ec8b6c6d917
luigi1015 <github@314ghz.com>
2014-01-19 19:02:36 -0600

    Wrote a little of the GUI.

--- src/net/codehobby/PowerballApp.java
+++ src/net/codehobby/PowerballApp.java
@@ -1,9 +1,39 @@
 package net.codehobby;
 
-public class PowerballApp {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
+public class PowerballApp extends JFrame
+{
+
+	private JPanel panel;
+
+	public PowerballApp()
+	{
+		initUI();
+	}
+
+	private void initUI()
+	{
+		//Set up the panel.
+		panel = new JPanel();
+		panel.setLayout( null );
+		getContentPane().add( panel );
+
+		setTitle( "PowerBall" );
+		setSize( 600, 600 );
+		setLocationRelativeTo( null );
+		setDefaultCloseOperation( EXIT_ON_CLOSE );//Set it to shut down once the close button is pressed.
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//The invokeLater puts the application on the Swing Event Queue to make sure all UI updates are concurrency safe.
+		SwingUtilities.invokeLater( new Runnable() {
+			public void run() {
+				PowerballApp pba = new PowerballApp();
+				pba.setVisible( true );
+			}
+		});
 
 	}
 

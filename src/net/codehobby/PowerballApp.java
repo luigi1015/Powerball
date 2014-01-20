91e6a30f6a7ea998590351db8f3a951bad85ba8c
luigi1015 <github@314ghz.com>
2014-01-20 17:50:35 -0600

    Added a list UI element.

--- src/net/codehobby/PowerballApp.java
+++ src/net/codehobby/PowerballApp.java
@@ -1,13 +1,20 @@
 package net.codehobby;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 
 public class PowerballApp extends JFrame
 {
-
+	private static final long serialVersionUID = 893436983339796805L;//Automatically generated serial version ID.
 	private JPanel panel;
+	private DefaultListModel<String> powerballNumListModel;
+	private JList<String> powerballNumList;
+	private JScrollPane powerballNumScrollPane;
 
 	public PowerballApp()
 	{
@@ -20,6 +27,21 @@ public class PowerballApp extends JFrame
 		panel = new JPanel();
 		panel.setLayout( null );
 		getContentPane().add( panel );
+
+		//Set up the list of Powerball numbers.
+		//First set up the list model.
+		powerballNumListModel = new DefaultListModel<String>();
+		powerballNumListModel.addElement( "Test1" );
+		powerballNumListModel.addElement( "Test2" );
+		//Then set up the JList
+		powerballNumList = new JList<String>( powerballNumListModel );
+		powerballNumList.setSelectedIndex( 0 );
+		//powerballNumList.addListSelectionListener( this );
+		powerballNumList.setVisibleRowCount( 5 );
+		//Then set up the scroll pane
+		powerballNumScrollPane = new JScrollPane( powerballNumList, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED );
+		powerballNumScrollPane.setBounds( 50, 50, 120, 120);
+		panel.add( powerballNumScrollPane );
 
 		setTitle( "PowerBall" );
 		setSize( 600, 600 );
